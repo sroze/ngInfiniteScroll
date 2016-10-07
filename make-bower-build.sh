e3a82eb9b735c6900c2b430bdf1b6b0274757344
Vikalp Jain <mevikalp@gmail.com>
2016-10-07 18:38:06 +0100

    Make bower builds from travis
    Minor script changes to point to original repo and master branch
    
    Review Fixes

---
+++ make-bower-build.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -e # Exit with nonzero exit code if anything fails
+
+SOURCE_BRANCH="master"
+TARGET_BRANCH="master"
+
+# Run only test cases if this is not a release
+if [ -z "$TRAVIS_TAG" ]; then
+    echo "Skipping deploy; just doing a build and running test cases"
+    npm test
+    exit 0
+fi
+
+npm test
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in id_rsa_nginfinite.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+BOWER_REPO='git@github.com:ng-infinite-scroll/ng-infinite-scroll-bower.git'
+
+BOWER_REPO_DIR='out'
+CWD="$PWD"
+
+git clone $BOWER_REPO $BOWER_REPO_DIR
+cd $BOWER_REPO_DIR
+git checkout $TARGET_BRANCH || git checkout -b $TARGET_BRANCH
+
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+# Copy all build content to the bower repo
+cp -r ../build .
+cp -r ../src .
+
+git add build
+git add src
+
+git commit -am "Release version $TRAVIS_TAG"
+git tag "$TRAVIS_TAG"
+git push origin $TARGET_BRANCH $TRAVIS_TAG
